02b7b7d4fee0f9689585be656f0dd833d1059468
Rory <awesomerortube@gmail.com>
2019-05-06 11:29:48 -0700

    did more stuff

--- Assets/Scripts/FollowThePath.cs
+++ Assets/Scripts/FollowThePath.cs
@@ -27,11 +27,11 @@ public class FollowThePath : MonoBehaviour
 
     private void Move()
     {
-        int total = 0;
+
+        int total = GetComponent<RollButton>().totalDice;
+        
         if (waypointIndex <= waypoints.Length - 1 && total >= 0)
-        {
-            
-        }
+       
         {
             transform.position = Vector2.MoveTowards(transform.position,
                 waypoints[waypointIndex].transform.position,
@@ -40,6 +40,7 @@ public class FollowThePath : MonoBehaviour
             if (transform.position == waypoints[waypointIndex].transform.position)
             {
                 waypointIndex += 1;
+                total--;
             }
         }
     }
--- Assets/Scripts/RollButton.cs
+++ Assets/Scripts/RollButton.cs
@@ -30,5 +30,4 @@ public class RollButton : MonoBehaviour
         }
     }
 
-  
 }
